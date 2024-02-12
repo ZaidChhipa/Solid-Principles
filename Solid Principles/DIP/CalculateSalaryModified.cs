e516082b5ad2e007c22327f5f16ac0f6e1f6f66c
Zaid.Chhipa <muhammadzaidchhipa@gmail.com>
2024-02-12 10:40:55 +0500

    commiting DIP

---
+++ Solid Principles/DIP/CalculateSalaryModified.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principles.DIP
+{
+    public class CalculateSalaryModified : ISalaryCalculate
+    {
+        public float CalculateSalary(float houtsworked, float hourlyrate)
+        {
+            return hourlyrate * houtsworked;
+        }
+    }
+}
---
+++ Solid Principles/DIP/EmployeeDetailsModified.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principles.DIP
+{
+    public class EmployeeDetailsModified
+    {
+        private readonly ISalaryCalculate _salarycaculate;
+
+        public float HoursWorked { get; set; }
+        public float HourlyRate { get; set; }
+
+        public EmployeeDetailsModified(ISalaryCalculate salaryCalculate)
+        {
+            _salarycaculate = salaryCalculate;
+        }
+
+        public float GetSalary()
+        {
+            return _salarycaculate.CalculateSalary(HoursWorked, HourlyRate);
+        }
+    }
+}
